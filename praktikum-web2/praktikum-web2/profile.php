80719cf8346e9d7cd440f34e59bc7780f6fccbad
CatABit2 <aldirpandi007@gmail.com>
2025-06-03 09:21:10 +0800

    Initial commit

---
+++ praktikum-web2/praktikum-web2/profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/saya.jpg" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2210010610</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">M. Aldi Ripandi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 05 Oktober 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0895 3542 09379</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Email :</strong></td>
+                        <td align="left" width="50%">aldirpandi007@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.facebook.com/aldi.rifandi.7" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/aldrpndi?igsh=bDF1NnFseGN4aTRv&utm_source=qr" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://x.com/alrpdi?s=11" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="https://www.linkedin.com" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
